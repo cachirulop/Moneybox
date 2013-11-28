f334e39486aa165cc48ebcba18f42f92a2849f5b
cachirulop <cachirulop@gmail.com>
2013-11-28 15:14:34 +0100

    OPEN - task 26: Add options to manage multiple moneybox http://github.com/cachirulop/Moneybox/issues/issue/26

--- Moneybox/src/com/cachirulop/moneybox/adapter/MoneyboxAdapter.java
+++ Moneybox/src/com/cachirulop/moneybox/adapter/MoneyboxAdapter.java
@@ -5,6 +5,7 @@ import java.util.List;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.RadioButton;
@@ -17,100 +18,157 @@ import com.cachirulop.moneybox.manager.MoneyboxesManager;
 /**
  * Class to show a moneybox information in the drawer menu.
  * 
- * It shows the description, the creation date of the moneybox and a 
- * radio button to select the moneybox.
+ * It shows the description, the creation date of the moneybox and a radio
+ * button to select the moneybox.
  * 
  * @author david
- *
+ * 
  */
 public class MoneyboxAdapter extends BaseAdapter {
-    /** Inflater to load the xml with the definition of the view */
-    private LayoutInflater _inflater;
-    
-    /** List of moneyboxes to be showed */
-    private List<Moneybox> _lstMoneyboxes = null;
+	/** Inflater to load the xml with the definition of the view */
+	private LayoutInflater _inflater;
 
-    /**
-     * Constructor that receives the context (parent) of the adapter.
-     * Creates the inflater to load the xml with the definition of the view.
-     * 
-     * @param context Parent of the adapter.
-     */
-    public MoneyboxAdapter(Context context) {
-        _inflater = LayoutInflater.from(context);
-    }
+	/** List of moneyboxes to be showed */
+	private List<Moneybox> _lstMoneyboxes = null;
+	
+	private int _currentPosition = -1;
+	private RadioButton _currentButton = null;
+	
 
-    /**
-     * Returns the number of items in the list of moneyboxes.
-     */
-    public int getCount() {
-        if (_lstMoneyboxes == null) {
-            refreshMoneyboxes();
-        }
+	/**
+	 * Constructor that receives the context (parent) of the adapter. Creates
+	 * the inflater to load the xml with the definition of the view.
+	 * 
+	 * @param context
+	 *            Parent of the adapter.
+	 */
+	public MoneyboxAdapter(Context context) {
+		_inflater = LayoutInflater.from(context);
+	}
 
-        return _lstMoneyboxes.size();
-    }
+	/**
+	 * Returns the number of items in the list of moneyboxes.
+	 */
+	public int getCount() {
+		if (_lstMoneyboxes == null) {
+			refreshMoneyboxes();
+		}
 
-    /**
-     * Returns the item of the specified position.
-     */
-    public Object getItem(int position) {
-        if (_lstMoneyboxes == null) {
-            refreshMoneyboxes();
-        }
+		return _lstMoneyboxes.size();
+	}
 
-        return _lstMoneyboxes.get(position);
-    }
+	/**
+	 * Returns the item of the specified position.
+	 */
+	public Object getItem(int position) {
+		if (_lstMoneyboxes == null) {
+			refreshMoneyboxes();
+		}
 
-    /**
-     * Returns the identifier of the item in the specified position.
-     * The identifier is the field IdMoneybox of the moneybox object.
-     */
-    public long getItemId(int position) {
-        if (_lstMoneyboxes == null) {
-            refreshMoneyboxes();
-        }
+		return _lstMoneyboxes.get(position);
+	}
 
-        return _lstMoneyboxes.get(position).getIdMoneybox();
-    }
+	/**
+	 * Returns the identifier of the item in the specified position. The
+	 * identifier is the field IdMoneybox of the moneybox object.
+	 */
+	public long getItemId(int position) {
+		if (_lstMoneyboxes == null) {
+			refreshMoneyboxes();
+		}
 
-    /**
-     * Returns the view to be showed in a row of the list.
-     * 
-     * The view is created from the layout moneybox_row in the folder res of
-     * the project.
-     * 
-     */
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = _inflater.inflate(R.layout.moneybox_row, null);
-        }
+		return _lstMoneyboxes.get(position).getIdMoneybox();
+	}
 
-        Moneybox m;
-        RadioButton rbMoneybox;
-        TextView txtCreationDate;
+	/**
+	 * Update the list of moneyboxes reading from the database with the method
+	 * {@link MoneyboxesManager#getAllMoneyboxes}
+	 */
+	public void refreshMoneyboxes() {
+		_lstMoneyboxes = MoneyboxesManager.getAllMoneyboxes();
 
-        m = _lstMoneyboxes.get(position);
-        
-        rbMoneybox = (RadioButton) convertView
-                .findViewById(R.id.rbMoneyboxRowMoneybox);
-        txtCreationDate = (TextView) convertView
-                .findViewById(R.id.txtMoneyboxRowCreationDate);
+		notifyDataSetChanged();
+	}
 
-        rbMoneybox.setText(m.getDescription());
-        rbMoneybox.setTag(m);
-        txtCreationDate.setText(m.getCreationDateFormatted());
-        
-        return convertView;
-    }
+	/**
+	 * Returns the view to be showed in a row of the list.
+	 * 
+	 * The view is created from the layout moneybox_row in the folder res of the
+	 * project.
+	 * 
+	 */
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		View view;
+		ViewHolder holder;
 
-    /**
-     * Update the list of moneyboxes reading from the database with the 
-     * method {@link MoneyboxesManager#getAllMoneyboxes}
-     */
-    public void refreshMoneyboxes() {
-        _lstMoneyboxes = MoneyboxesManager.getAllMoneyboxes();
-        
-        notifyDataSetChanged();
-    }
+		view = convertView;
+
+		if (view == null) {
+			view = _inflater.inflate(R.layout.moneybox_row, null);
+			
+			holder = new ViewHolder();
+			holder.btn = (RadioButton) view.findViewById(R.id.rbMoneyboxRowMoneybox);
+			holder.txt = (TextView) view.findViewById(R.id.txtMoneyboxRowCreationDate);
+			
+			view.setTag(holder);
+		}
+		else {
+			holder = (ViewHolder) view.getTag();
+		}
+		
+		holder.btn.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (position != _currentPosition && _currentButton != null) {
+					_currentButton.setChecked(false);
+				}
+				
+				_currentPosition = position;
+				_currentButton = (RadioButton) v;
+			}
+		});
+		
+		if (_currentPosition != position) {
+			holder.btn.setChecked(false);
+		}
+		else {
+			holder.btn.setChecked(true);
+			if (_currentButton != null && holder.btn != _currentButton) {
+				_currentButton = holder.btn;
+			}
+		}
+		
+		Moneybox m; 
+		 
+		m = _lstMoneyboxes.get(position);
+		
+		holder.btn.setText(m.getDescription()); 
+		holder.btn.setTag(m);
+		holder.txt.setText(m.getCreationDateFormatted());
+		
+		return view;
+	}
+
+	/*
+	 * public View getView(int position, View convertView, ViewGroup parent) {
+	 * if (convertView == null) { convertView =
+	 * _inflater.inflate(R.layout.moneybox_row, null); }
+	 * 
+	 * Moneybox m; RadioButton rbMoneybox; TextView txtCreationDate;
+	 * 
+	 * m = _lstMoneyboxes.get(position);
+	 * 
+	 * rbMoneybox = (RadioButton) convertView
+	 * .findViewById(R.id.rbMoneyboxRowMoneybox); txtCreationDate = (TextView)
+	 * convertView .findViewById(R.id.txtMoneyboxRowCreationDate);
+	 * 
+	 * rbMoneybox.setText(m.getDescription()); rbMoneybox.setTag(m);
+	 * txtCreationDate.setText(m.getCreationDateFormatted());
+	 * 
+	 * return convertView; }
+	 */
+
+	private class ViewHolder {
+		RadioButton btn;
+		TextView txt;
+	}
 }
